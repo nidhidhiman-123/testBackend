3559df20aa63162f9adba96aa58ec7b6cbdee2ec
Nidhi Dhiman <104757239+nidhidhiman-123@users.noreply.github.com>
2023-02-08 19:46:52 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -24,15 +24,15 @@ router.get('/all_post', auth, postController.allpost);
 router.delete('/delete_post/:id', postController.deletepost);
 router.post('/add_user', newuserController.adduser);
 router.post('/add_leave', postController.add_leaves);
-// router.post('/apply_leave', auth, applyleaveController.apply);
+router.post('/apply_leave', auth, applyleaveController.apply);
 router.get('/all_leave', postController.allleave);
 router.get('/get_apply_leaves', applyleaveController.getapply_leaves);
 router.post('/update_leave/:id', applyleaveController.update_leave);
 router.put('/cancel_leave/:id', applyleaveController.cancel_leave);
-// router.get('/single_user_apply_leave', auth, applyleaveController.single_user_apply_leave);
-// router.post('/like/:id', auth, postController.like);
+router.get('/single_user_apply_leave', auth, applyleaveController.single_user_apply_leave);
+router.post('/like/:id', auth, postController.like);
 router.get('/allcomment', postController.allcomment);
-// router.get('/all', auth, newuserController.all)
+router.get('/all', auth, newuserController.all)
 router.get('/all_employee', newuserController.all_employee)
 
 module.exports = router;
